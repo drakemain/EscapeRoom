b1e4e283f9bfa5987a7fcdf410d83b2d4a3d241c
drakemain <demain1990@gmail.com>
2017-05-19 00:01:11 -0700

    door open component

---
+++ Source/EscapeRoom/OpenDoor.cpp
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EscapeRoom.h"
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+	SetPositions();
+	this->ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
+}
+
+// Set the open and close positions
+void UOpenDoor::SetPositions()
+{
+	this->ClosedPosition = GetOwner()->GetActorRotation();
+	this->OpenPosition = this->ClosedPosition;
+	this->OpenPosition.Add(0.f, this->OpenAngle, 0.f);
+}
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (this->Trigger && this->Trigger->IsOverlappingActor(this->ActorThatOpens))
+	{
+		Open();
+	}
+	else 
+	{
+		Close();
+	}
+}
+
+void UOpenDoor::Open()
+{
+	if (GetOwner()->GetActorRotation() != this->OpenPosition)
+	{
+		GetOwner()->SetActorRelativeRotation(this->OpenPosition);
+	}
+}
+
+void UOpenDoor::Close()
+{
+	if (GetOwner()->GetActorRotation() != this->ClosedPosition)
+	{
+		GetOwner()->SetActorRelativeRotation(this->ClosedPosition);
+	}
+}
---
+++ Source/EscapeRoom/OpenDoor.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "OpenDoor.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ESCAPEROOM_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	void SetPositions();
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	void Open();
+	void Close();
+
+		
+private:
+	UPROPERTY(EditAnywhere)
+	float OpenAngle = 90.f;
+	FRotator ClosedPosition;
+	FRotator OpenPosition;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* Trigger;
+	AActor* ActorThatOpens;
+};
